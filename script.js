31d2289d180d2146bf4454857eabcbaf1bb5f1a6
baldogy101 <baldogy01@gmail.com>
2025-02-24 08:27:45 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,14 @@
+// Load Header
+document.addEventListener("DOMContentLoaded", function () {
+    fetch("header.html")
+        .then(response => response.text())
+        .then(data => {
+            document.getElementById("header-placeholder").innerHTML = data;
+        })
+        .catch(error => console.error("Error loading header:", error));
+});
+
+// subscription calculator
 document.getElementById("calculateBtn").addEventListener("click", function() {
     const numUsers = parseInt(document.getElementById("numUsers").value);
     const duration = parseInt(document.getElementById("duration").value);
